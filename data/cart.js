19838b4f2f0a34a0723e4e990ea86a5a9613e66e
naman-gudhka <naman.gudhka19@gmail.com>
2026-07-13 19:27:14 +0530

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 },{
@@ -23,4 +23,14 @@ export function addToCart(productId){
       quantity
     }); // Used the shorthand property.
   }
+}
+
+export function removeFromCart(productID){
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if(cartItem.productId != productID){
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
 }
