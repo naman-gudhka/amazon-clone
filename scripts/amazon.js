96e5336bc0d68d9a51ee6b9347a7893a8ebc24b1
naman-gudhka <naman.gudhka19@gmail.com>
2026-08-04 09:09:32 +0530

    Load Products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,77 +1,76 @@
 import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {products, loadProducts} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
+
+loadProducts(renderProductsGrid);
 
 updateCartQuantity();
 
-let productsHTML = '';
+function renderProductsGrid(){
 
-products.forEach((product) => {
-  productsHTML += `
-    <div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image"
-          src="${product.image}">
-      </div>
+  let productsHTML = '';
 
-      <div class="product-name limit-text-to-2-lines">
-        ${product.name}
-      </div>
-
-      <div class="product-rating-container">
-        <img class="product-rating-stars"
-          src="${product.getStarsUrl()}">
-        <div class="product-rating-count link-primary">
-          ${product.rating.count}
+  products.forEach((product) => {
+    productsHTML += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image"
+            src="${product.image}">
         </div>
+
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+          <img class="product-rating-stars"
+            src="${product.getStarsUrl()}">
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
+
+        <div class="product-price">
+          ${product.getPrice()}
+        </div>
+
+        <div class="product-quantity-container">
+          <select class="js-quantity-selector-${product.id}">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        ${product.extraInfoHTML()}
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart js-added-to-cart-${product.id}">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary js-add-to-cart"
+        data-product-id="${product.id}">
+          Add to Cart
+        </button>
       </div>
+    `;
+  });
 
-      <div class="product-price">
-        ${product.getPrice()}
-      </div>
-
-      <div class="product-quantity-container">
-        <select class="js-quantity-selector-${product.id}">
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-
-      ${product.extraInfoHTML()}
-
-      <div class="product-spacer"></div>
-
-      <div class="added-to-cart js-added-to-cart-${product.id}">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
-
-      <button class="add-to-cart-button button-primary js-add-to-cart"
-      data-product-id="${product.id}">
-        Add to Cart
-      </button>
-    </div>
-  `;
-});
 
 // This comment is for line #48:
 // polymorphism: The extraInfoHTML method is called on the product object, which can be an instance of either the Product class or the Clothing class. Depending on the actual type of the product, the appropriate extraInfoHTML method will be executed. This allows us to handle different types of products in a uniform way without needing to know their specific types in advance.
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
-function updateCartQuantity(){
-  const cartQuantity = calculateCartQuantity();
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
-}
 
 const addedMessageTimeouts = {};
 
@@ -95,3 +94,11 @@ document.querySelectorAll('.js-add-to-cart')
       });
     });
   });
+
+}
+
+function updateCartQuantity(){
+  const cartQuantity = calculateCartQuantity();
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,11 @@ import renderCheckoutHeader from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../../data/cart-class.js';
-import '../../data/backend-practice.js';
+// import '../../data/backend-practice.js';
+import {loadProducts} from '../data/products.js';
 
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
